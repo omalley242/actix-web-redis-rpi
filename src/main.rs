703b00fe4d4b70acc5a3d34a5fe2de728518a66b
omalley242 <omalley242@gmail.com>
2023-04-15 00:00:10 +0100

    testing redis query

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer, middleware::Logger, web::Bytes, get, HttpResponse, web};
+use actix_web::{App, HttpServer, HttpRequest, middleware::Logger, web::Bytes, get, HttpResponse, web};
 use awc;
 use actix_files as fs;
 use redis::{Connection, RedisError};
@@ -65,7 +65,7 @@ async fn update_redis(server: &mut Connection, data: String) -> Result<bool, Box
     Ok(true)
 }
 
-async fn test_func() -> Result<HttpResponse, Box<dyn std::error::Error>> {
+async fn poll_update() -> Result<HttpResponse, Box<dyn std::error::Error>> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut server = client.get_connection()?;
     let update = do_i_update(&mut server).await?;
@@ -77,6 +77,15 @@ async fn test_func() -> Result<HttpResponse, Box<dyn std::error::Error>> {
     Ok(HttpResponse::Ok().finish())
 }
 
+#[get("/query/{id}")]
+async fn redis_query(req: HttpRequest) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut server = client.get_connection()?;
+    let query_id = req.match_info().get("id").unwrap().to_string();
+    let res: String = redis::cmd("JSON.GET").arg("json").arg(query_id).query(&mut server)?;
+    Ok(HttpResponse::Ok().body(res))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //start the logger
@@ -86,7 +95,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
         App::new()
         .wrap(Logger::default())
-        .route("/test", web::get().to(test_func))
+        .route("/test", web::get().to(poll_update))
+        .service(redis_query)
         .service(fs::Files::new("/", "./static/home_page")
             .show_files_listing()
             .index_file("home_page.html")
