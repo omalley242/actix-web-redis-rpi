480c43305a36d5743d03fcfa54fcf657f0986ef5
omalley242 <omalley242@gmail.com>
2023-04-14 20:21:22 +0100

    fix connection refused error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer, middleware::Logger, web::Bytes, get, HttpResponse};
+use actix_web::{App, HttpServer, middleware::Logger, web::Bytes, get, HttpResponse, web};
 use awc;
 use actix_files as fs;
 use redis::{Connection, RedisError};
@@ -25,7 +25,7 @@ struct json_format {
 const API_PING_TIME_SECS: u64 = 10;
 
 async fn api_request() -> Result<Bytes, Box<dyn std::error::Error>>{
-    info!{"requesting tfl api data"};
+    info!("requesting tfl api data");
     let client = awc::Client::default();
     let req = client.get("https://api.tfl.gov.uk/BikePoint/");
     let mut res = req.send().await?;
@@ -33,13 +33,13 @@ async fn api_request() -> Result<Bytes, Box<dyn std::error::Error>>{
 }   
 
 async fn deserialize(data: Bytes) -> Result<Vec::<json_format>, Box<dyn std::error::Error>> {
-    info!{"deserializing json data"};
+    info!("deserializing json data");
     let json_data: Vec::<json_format> = serde_json::from_slice(&data)?;
     Ok(json_data)
 }
 
 async fn do_i_update(server: &mut Connection) -> Result<bool, Box<dyn std::error::Error>> {
-    info!{"checking time from redis"};
+    info!("checking time from redis");
     let res: u64 = redis::cmd("GET").arg("api_timestamp").query(server)?;
     if res - std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs() > API_PING_TIME_SECS {
         return Ok(true);
@@ -48,7 +48,7 @@ async fn do_i_update(server: &mut Connection) -> Result<bool, Box<dyn std::error
 }
 
 async fn update_redis(server: &mut Connection, data: Vec::<json_format>) -> Result<bool, Box<dyn std::error::Error>> {
-    info!{"updating redis json data"};
+    info!("updating redis json data");
     let _: () = redis::cmd("SET").arg("api_timestamp").arg(std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs()).query(server)?;
     let _: Vec<Result<redis::Value, RedisError>> = data.iter().map(|x| 
         redis::pipe()
@@ -64,8 +64,7 @@ async fn update_redis(server: &mut Connection, data: Vec::<json_format>) -> Resu
     Ok(true)
 }
 
-#[get("/test")]
-async fn test() -> Result<HttpResponse, Box<dyn std::error::Error>>{
+async fn test_func() -> Result<HttpResponse, Box<dyn std::error::Error>> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut server = client.get_connection()?;
     let update = do_i_update(&mut server).await?;
@@ -86,12 +85,12 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
         App::new()
         .wrap(Logger::default())
+        .route("/test", web::get().to(test_func))
         .service(fs::Files::new("/", "./static/home_page")
             .show_files_listing()
             .index_file("home_page.html")
             .use_last_modified(true),
         )
-        .service(test)
     )
     //bind it to the open port on 8080 (http not ssl)
     .bind(("0.0.0.0", 8080))?
