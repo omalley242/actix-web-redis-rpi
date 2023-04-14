87640e52a28b553c6999c13e55d0c4a04a6b6db0
omalley242 <omalley242@gmail.com>
2023-04-14 19:17:17 +0100

    add redis time query, add database basic updating

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{App, HttpServer, middleware::Logger};
+use std::string;
+
+use actix_web::{App, HttpServer, middleware::Logger, web::Bytes};
 use awc;
 use actix_files as fs;
+use redis::{Connection, RedisError, RedisResult};
 use serde::{Deserialize};
 use log::{info};
 
@@ -16,37 +19,63 @@ struct json_format {
     url: String,
     commonName: String,
     placeType: String,
-    additionalProperties: Vec::<serde_json::Value>,
+    additionalProperties: String,
     lat: f64,
     lon: f64,
 }
 
-async fn tfl_api_request_handler() -> Result<String, Box<dyn std::error::Error>>{
+const api_ping_time_secs: u64 = 10;
+
+async fn api_request() -> Result<Bytes, Box<dyn std::error::Error>>{
     //make http request and handle response with api
     let client = awc::Client::default();
     let req = client.get("https://api.tfl.gov.uk/BikePoint/");
     let mut res = req.send().await?;
-    let body = res.body().limit(2500000).await?;
-    //Deserialize ----
-    let value_enum: serde_json::Value = serde_json::from_slice(&body)?;
-    let json_stations_list = value_enum.as_array().unwrap();
-    for station in json_stations_list{
-        let Data: json_format = serde_json::from_value(station.clone())?;
-        //Load Deserialized data into Redis
-        info!("{} : {}", Data.commonName, Data.additionalProperties[2]["value"]);
-    }
-    Ok("Fetched the tfl station data".to_string())
+    Ok(res.body().limit(2500000).await?)
 }   
+
+async fn deserialize(data: Bytes) -> Result<json_format, Box<dyn std::error::Error>> {
+    let json_data: json_format = serde_json::from_slice(&data)?;
+    Ok(json_data)
+}
+
+async fn do_i_update(server: &mut Connection) -> Result<bool, Box<dyn std::error::Error>> {
+    let res: u64 = redis::cmd("GET").arg("api_timestamp").query(server)?;
+    if res - std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs() > api_ping_time_secs {
+        return Ok(true);
+    }
+    Ok(false)
+}
+
+async fn update_redis(server: &mut Connection, data: Vec::<json_format>) -> Result<bool, Box<dyn std::error::Error>> {
+    let _: () = redis::cmd("SET").arg("api_timestamp").arg(std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs()).query(server)?;
+    let checksums: Vec<Result<redis::Value, RedisError>> = data.iter().map(|x| 
+         redis::pipe()
+        .cmd("SET").arg(&x.id).arg(&x.url)
+        .cmd("SET").arg(&x.id).arg(&x.commonName)
+        .cmd("SET").arg(&x.id).arg(&x.placeType)
+        .cmd("JSON.SET").arg(&x.id).arg("$").arg(&x.additionalProperties)
+        .cmd("SET").arg(&x.id).arg(&x.lat)
+        .cmd("SET").arg(&x.id).arg(&x.lon)
+        .query(server)
+    ).collect();
+    
+    Ok(true)
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //start the logger
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    info!("{}", tfl_api_request_handler().await.unwrap());
 
     //Create a server for each thread
     HttpServer::new(|| 
+        //Check if data is too old
+
+        //if too old run api request and update redis
+
         //create the httpserver details with the app builder
         App::new()
         .wrap(Logger::default())
